159e145046a842e8bb48f8b97f5986ef25232d52
ivy-reis <ivyris6666@gmail.com>
2026-05-04 16:42:10 -0300

    chore: atualiza identidade global do git e realiza commit

--- DTOs/AberturaDTO.cs
+++ DTOs/AberturaDTO.cs
@@ -5,3 +5,5 @@ public class AberturaDTO
     public string Nome { get; set; } = string.Empty;
     public string Cor { get; set; } = string.Empty; // "Brancas" ou "Pretas"
 }
+
+// commit sendo realizado novamente pois não tinha configurado o email
